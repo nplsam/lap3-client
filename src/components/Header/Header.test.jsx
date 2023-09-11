a803ca74f73d4a7cd5426d5f9b7733bb42b908f2
nplsam <psammerrick@gmail.com>
2023-09-11 18:56:51 +0100

    2 tests performed on Header

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { describe, it, expect, beforeEach, afterEach, jest } from 'vitest'
+import { screen, render, cleanup } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'; // with this we can specify fake history to allow us to test things
+
+import * as matchers from '@testing-library/jest-dom/matchers'
+expect.extend(matchers)
+
+import Header from '.'
+
+describe('Header Component', () => {
+    beforeEach(() => {
+        render(
+            <MemoryRouter>
+                <Header />
+            </MemoryRouter>
+        )
+    })
+
+    afterEach(() => {
+        cleanup()
+    })
+
+    it('display a Header with 4 children', () => {
+        const nav = screen.getByRole('list')
+
+        expect(nav).toBeInTheDocument()
+        expect(nav.childNodes.length).toBe(4)
+    })
+
+    it('displays a "Home" link', () => {
+        const homeLink = screen.getByText('Home')
+
+        expect(homeLink).toBeInTheDocument()
+    })
+});
