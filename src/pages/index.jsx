3cf0e74aedfe3e30501e1157a8ad0b66bc6cd4db
Stephanie <stephf202@gmail.com>
2023-09-12 10:08:24 +0100

    add timer page, useContext, still need to add styleing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as HomePage } from './HomePage'
 export { default as NotFoundPage } from './NotFoundPage'
 export { default as NotesPage } from './NotesPage'
+export { default as TimerPage } from './TimerPage'
