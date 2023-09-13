abba432b4aa766f3fcfad0723bdd6b35bac45491
Valentin Abrutin <abrutinvalic@gmail.com>
2023-09-13 01:31:54 +0100

    fully styled calendar + made preparation for tasks functionality

--- src/components/PlannerCalendar/index.jsx
+++ src/components/PlannerCalendar/index.jsx
@@ -7,9 +7,11 @@ const PlannerCalendar = ({ tasks, setTasks }) => {
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear())
     const [currentMonthArr, setCurrentMonthArr] = useState([])
     const [currentMonthStart, setCurrentMonthStart] = useState([])
+    const [currentMonthEnd, setCurrentMonthEnd] = useState([])
 
     const currentDay = new Date().getDate()
     const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
     const nextMonth = () => {
         if (currentMonth === 11) {
@@ -38,24 +40,35 @@ const PlannerCalendar = ({ tasks, setTasks }) => {
 
         // Set currentMonthLength
         const monthLenght = new Date(currentYear, currentMonth + 1, 0).getDate()
-        const monthArr = new Array(monthLenght).fill(0)
+        const monthArr = new Array(monthLenght).fill([])
 
         tasks.forEach(task => {
             const taskDate = new Date(task.date)
 
             if(taskDate.getFullYear() === currentYear && taskDate.getMonth() === currentMonth) {
-                monthArr[taskDate.getDate() - 1] = task
+                monthArr[taskDate.getDate() - 1] = [...monthArr[taskDate.getDate() - 1], task]
             }
         });
 
         setCurrentMonthArr(monthArr)
 
-    }, [currentMonth])
+        // Set endingDate
+        let endingDate = new Date(currentYear, currentMonth, monthLenght).getDay() - 1
+        if (endingDate === -1 ) {
+            endingDate = 6
+        }
+        setCurrentMonthEnd(new Array(6 - endingDate).fill(0))
+
+    }, [currentMonth, tasks])
 
   return (
     <div id='PlannerCalendar-container'>
-        <button onClick={nextMonth}>Previous</button>
-        <button onClick={prevMonth}>Next</button>
+        <div id='PlannerCalendar-controls'>
+            <button onClick={prevMonth}>Previous</button>
+            <div>{months[currentMonth]} {currentYear}</div>
+            <button onClick={nextMonth}>Next</button>
+        </div>
+        
         <div id='PlannerCalendar'>
             <div className='dayname'>Mon</div>
             <div className='dayname'>Tue</div>
@@ -68,10 +81,18 @@ const PlannerCalendar = ({ tasks, setTasks }) => {
                 <div className='day' key={index}>
                 </div>
             ))}
-            {currentMonthArr.map((data, index) => (
+            {currentMonthArr.map((tasks, index) => (
                 <div className='day' key={index}>
                     <div>{index + 1}</div>
-                    <div className=''>{data.content}</div>
+                    <div className='item'>
+                        {tasks.map((task, index) => (
+                            <div className='' key={index}>{task.content}</div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+            {currentMonthEnd.map((data, index) => (
+                <div className='day' key={index}>
                 </div>
             ))}
         </div>
