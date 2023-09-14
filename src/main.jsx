e49961c63e26fd5e487d3dd63346f2f13b21fcda
nplsam <psammerrick@gmail.com>
2023-09-14 13:59:01 +0100

    fixed plannercontexts pathing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 import { TimerProvider } from './contexts/TimerContext'
 import { NotesProvider } from './contexts/NotesContext'
 import { AuthProvider } from './contexts/AuthContext'
-import { PlannerProvider } from './contexts/PlannerProvider'
+import { PlannerProvider } from './contexts/PlannerContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
