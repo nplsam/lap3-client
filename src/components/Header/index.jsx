bceacd4cc240607657e6551429c6b4d2742fa1fc
Valentin Abrutin <abrutinvalic@gmail.com>
2023-09-14 16:48:01 +0100

    fixed logout issue

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import '../../assets/css/header.css';
 import { useAuth } from '../../contexts/AuthContext';
+import axios from 'axios';
 
 const styles = ({ isActive }) => ({ textDecoration: isActive ? 'underline' : 'none' });
 
 const Header = () => {
   const { isLoggedIn, setIsLoggedIn, setUsername } = useAuth();
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    localStorage.removeItem('token');
-    // Instead of login, I will delete username on logout (Valentin)
-    setUsername('');
+  const handleLogout = async () => {
+    try {
+      const response = await axios.delete('http://localhost:5000/auth/logout', {
+        headers:{Authorization: localStorage.token}
+      });
+
+      if (response.status == 200) {
+        setIsLoggedIn(false);
+        localStorage.removeItem('token');
+        // Instead of login, I will delete username on logout (Valentin)
+        setUsername('');
+      } else {
+        throw new Error('Failed to logout on server side. Try again.')
+      }
+
+    } catch (error) {
+      console.log('Failed to logout: ' + error)
+    }
   };
 
   return (
